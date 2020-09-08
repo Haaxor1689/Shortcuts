939964c68be7a93acd73049da7ca1a68483e2a6b
Haaxor1689 <betkomaros@gmail.com>
2020-09-08 17:12:16 +0200

    Updates

---
+++ precache-manifest.d078f140000e0863505e4ca23c7fb41b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570ef9bc8b764e6d994f1481202419e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbd32e08aadf252c9cfa",
+    "url": "/static/js/2.3283a226.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3283a226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b42fa62a1cf591612fc",
+    "url": "/static/js/main.24b43099.chunk.js"
+  },
+  {
+    "revision": "bae492c776d6e417a041",
+    "url": "/static/js/runtime-main.1b461360.js"
+  }
+]);
